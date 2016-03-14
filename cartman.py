6c3d3ebe5d268436136badf05801bcae21de27d2
nimeshkverma <nimesh.verma@craftsvilla.com>
2016-03-14 22:39:54 +0530

    First Commit with Bare minimum backbone of Cartman

---
+++ cartman.py
@@ -0,0 +1,40 @@
+import redis
+import config
+from serializer import Serializer
+from redis_connection import RedisConnection
+
+
+class Cart(object):
+
+    """docstring for Cart"""
+
+    def __init__(self, user_id):
+        # Init function to check weather the cart dictionary exists in redis
+        # this can also be done via user every time he uses the library
+        self.user_id = user_id
+        self.redis_cart_init(user_id)
+
+    def redis_cart_init(self):
+        if not RedisConnection.hexists(config.CART_CONFIG["hash_name"], self.user_id):
+            RedisConnection.hset(
+                config.CART_CONFIG["hash_name"], self.user_id, {})
+
+    def __cart_dict(self):
+        cart_string = RedisConnection.hget(
+            config.CART_CONFIG["hash_name"], self.user_id)
+        return Serializer.loads(cart_string)
+
+    def __product_dict(self, product_price, qty, **kwargs):
+        return {
+            "product_price": product_price,
+            "qty": qty
+        }.update(kwargs)
+
+    def add(self, product_id, product_price, qty=1, **kwargs):
+        cart_dict = self.__cart_dict()
+        cart_dict[product_id] = self.__product_dict(product_id, qty, kwargs)
+        RedisConnection.hset(
+            config.CART_CONFIG["hash_name"], self.user_id, Serializer.dumps(cart_dict))
+
+    def remove(self, product_id):
+        pass
---
+++ redis_connection.py
@@ -0,0 +1,22 @@
+import redis
+from decorators import singleton, raise_exception
+from exception import ErrorMessage
+from config import REDIS
+
+
+@singleton
+class RedisConnection(object):
+
+    @raise_exception("Redis connection can't be established due to Error: ")
+    def get_redis_connections(self):
+        REDIS_SERVER = REDIS.get('SERVER')
+        REDIS_PORT = REDIS.get('PORT')
+        if REDIS_SERVER and REDIS_PORT:
+            REDIS_DB = REDIS['DB'] if REDIS.get('DB') else 0
+            REDIS_PASSWORD = REDIS['PASSWORD'] if REDIS.get(
+                'PASSWORD') else None
+            redis_connection = redis.Redis(
+                host=REDIS_SERVER, port=REDIS_PORT, db=REDIS_DB, password=REDIS_PASSWORD)
+            return redis_connection
+        else:
+            raise ErrorMessage("REDIS_SERVER or REDIS_PORT not provided")
